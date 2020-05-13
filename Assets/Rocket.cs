d3761e6781214c3c2ca0ab17715421fd54a48185
Tommy Malone <tmm3746@yahoo.com>
2020-05-12 20:30:06 -0400

    Basic rocket and starting scene setup. Input is being processed (not through Unity's virtual input system) and the player is able to move around the map. Main camera is still fixed.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+    [SerializeField] float thrustForce = 10f;
+    [SerializeField] float rcsThrust = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }    
+
+    void ProcessInput()
+    {
+        ProcessThrust();
+        ProcessRCS();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * thrustForce * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void ProcessRCS()
+    {
+        rigidbody.freezeRotation = true;    //Manually takes control of the rotation, getting rid of any lingering physics based rotation.
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * rotationThisFrame);
+        }
+
+        rigidbody.freezeRotation = false;    //Allows for physics based rotation once more.
+    }
+}
